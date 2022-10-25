c531e5dd039863c186886d8f69d98e218074cdd8
Thomas Conger <t.e.conger@gmail.com>
2022-10-25 12:17:54 -0700

    added bonus to employee and manager

--- inheritance/employee.rb
+++ inheritance/employee.rb
@@ -1,10 +1,16 @@
 class Employee
 
+    attr_accessor :salary
+
     def initialize(name, title, salary, boss)
         @name, @title, @salary, @boss = name, title, salary, boss
-        unless @boss.nil? 
+        unless @boss.nil?
             @boss.employees << self
         end
+    end
+
+    def bonus(multiplier=0)
+        bonus = self.salary * multiplier
     end
 
 
--- inheritance/manager.rb
+++ inheritance/manager.rb
@@ -2,12 +2,20 @@ require_relative "employee"
 
 class Manager < Employee
     attr_accessor :employees
+
     def initialize(name, title, salary, boss = nil)
         super
         @employees = []
     end
 
-    
+    def bonus(multiplier=1)
+      sum = 0
+      @employees.each do |employee|
+        sum += employee.salary
+      end
+      bonus = sum * multiplier
+    end
+
 
 
 
