880750f014d8421bfc4be47626f47165ec4e3248
Dylan Gavin <dyl.gavin@gmail.com>
2022-10-26 11:31:32 -0700

    reorganized file setup

--- chess/phase_1/piece.rb
+++
@@ -1,9 +0,0 @@
-
-class Piece
-
-  def initialize(name)
-    @name = name
-  end
-
-
-end
--- chess/pieces/king.rb
+++ chess/pieces/king.rb
@@ -1,5 +1,5 @@
 require_relative "piece"
-require_relative "stepable"
+require_relative "../modules/stepable"
 
 class King < Piece
 
